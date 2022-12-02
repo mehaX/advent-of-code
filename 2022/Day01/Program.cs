8df809d40557c3c5a54f7c869f12a5122dc4c5af
Melos Hasanaj <melos.hasanaj@gmail.com>
2022-12-02 16:16:16 +0100

    day1, both parts

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿static List<List<int>> GetElves()
+{
+    var inputs = File.ReadAllText("input.txt");
+
+    var segments = inputs.Split("\r\n\r\n");
+
+    return segments
+        .Select(segment => segment
+            .Split("\n")
+            .Select(int.Parse)
+            .ToList())
+        .ToList();
+}
+
+static int Part1()
+{
+    var elves = GetElves();
+
+    return elves.Select(calories => calories.Sum()).OrderDescending().First();
+}
+
+static int Part2()
+{
+    var elves = GetElves();
+
+    return elves.Select(calories => calories.Sum()).OrderDescending().Take(3).Sum();
+}
+
+Console.WriteLine("Part1: " + Part1());
+Console.WriteLine("Part2: " + Part2());
