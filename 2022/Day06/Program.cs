a2746ab76b3ac2baedb66277b74620712122c1d1
Melos Hasanaj <melos.hasanaj@gmail.com>
2022-12-06 08:30:37 +0100

    add day6

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Part(int count)
+{
+    var input = File.ReadAllText("input.txt");
+    var index = count - 1;
+    while (index < input.Length)
+    {
+        if (input.Skip(index - (count - 1)).Take(count).Distinct().Count() == count)
+        {
+            break;
+        }
+        
+        index++;
+    }
+
+    index++;
+
+    return index;
+}
+Console.WriteLine("Part 1: " + Part(4));
+Console.WriteLine("Part 2: " + Part(14));
