f8694469ec996f161122822534b85fa57f10ee84
Melos Hasanaj <melos.hasanaj@gmail.com>
2023-12-13 14:08:38 +0100

    added 2023 with days 1 and 2

---
+++ 2023/Day01/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.RegularExpressions;
+
+var rows = File.ReadAllLines("input.txt").ToList();
+
+string RemoveLetters(string input)
+{
+    return new Regex("[a-zAZ]+").Replace(input, "");
+}
+
+int Part1()
+{
+    return rows
+        .Select(RemoveLetters)
+        .Select(row => row[0] + "" + row[^1])
+        .Select(int.Parse)
+        .Sum();
+}
+
+int Part2()
+{
+    var numbers = new Dictionary<string, int>()
+    {
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 },
+    };
+
+    return rows.Select(row =>
+        {
+            var originalRow = row.Clone();
+            var firstLetter = "";
+            var secondLetter = "";
+
+            for (var i = 0; i < row.Length; i++)
+            {
+                if (row[i] >= '0' && row[i] <= '9')
+                {
+                    firstLetter = row[i].ToString();
+                    break;
+                }
+
+                foreach (var number in numbers)
+                {
+                    if (i + number.Key.Length <= row.Length && row.Substring(i, number.Key.Length).Equals(number.Key))
+                    {
+                        firstLetter = number.Value.ToString();
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(firstLetter))
+                {
+                    break;
+                }
+            }
+            
+
+            for (var i = row.Length - 1; i >= 0; i--)
+            {
+                if (row[i] >= '0' && row[i] <= '9')
+                {
+                    secondLetter = row[i].ToString();
+                    break;
+                }
+
+                foreach (var number in numbers)
+                {
+                    if (i + number.Key.Length <= row.Length && row.Substring(i, number.Key.Length).Equals(number.Key))
+                    {
+                        secondLetter = number.Value.ToString();
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(secondLetter))
+                {
+                    break;
+                }
+            }
+
+            var combined = firstLetter + secondLetter;
+            var result = int.Parse(combined);
+
+            // Console.WriteLine($"{row} {combined} {result}");
+            
+            return result;
+        })
+        .Sum();
+}
+
+Console.WriteLine("Part1: " + Part1());
+Console.WriteLine("Part2: " + Part2());
---
+++ 2023/Day02/Program.cs
@@ -0,0 +1,77 @@
+﻿var games = File.ReadAllLines("input.txt").Select(Game.Deserialize).ToList() ?? throw new Exception("");
+
+
+int Part1()
+{
+    var ids = new List<int>();
+    foreach (var game in games)
+    {
+        var redCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "red").Value);
+        var greenCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "green").Value);
+        var blueCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "blue").Value);
+
+        if (redCubes <= 12 && greenCubes <= 13 && blueCubes <= 14)
+        {
+            ids.Add(game.Index);
+        }
+    }
+
+    return ids.Sum();
+}
+
+int Part2()
+{
+    return games.Select(game =>
+    {
+        var redCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "red").Value);
+        var greenCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "green").Value);
+        var blueCubes = game.Sets.Max(s => s.Cubes.FirstOrDefault(c => c.Key == "blue").Value);
+
+        return redCubes * greenCubes * blueCubes;
+    }).Sum();
+}
+
+Console.WriteLine("Part1 " + Part1());
+Console.WriteLine("Part2 " + Part2());
+
+class Game
+{
+    public int Index;
+    public List<Set> Sets = new();
+
+    public static Game Deserialize(string row)
+    {
+        var game = new Game();
+        var chunks = row.Split(":").ToArray();
+        game.Index = int.Parse(chunks[0].Replace("Game ", ""));
+        game.Sets = chunks[1].Trim().Split("; ").Select(Set.DeserializeSet).ToList();
+
+        return game;
+    }
+}
+
+class Set
+{
+    public Dictionary<string, int> Cubes = new();
+
+    public static Set DeserializeSet(string input)
+    {
+        var set = new Set();
+
+        var chunks = input.Split(", ").ToList();
+        foreach (var chunk in chunks)
+        {
+            var cube = DeserializeCube(chunk);
+            set.Cubes.Add(cube.Item1, cube.Item2);
+        }
+
+        return set;
+    }
+
+    public static (string, int) DeserializeCube(string input)
+    {
+        var chunks = input.Split(" ").ToArray();
+
+        return (chunks[1], int.Parse(chunks[0]));
+    }
+}
